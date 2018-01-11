c2b13818253bfd4dc8a047c5648526a297e04e96
Christoffer Nygren <christoffernygren90@hotmail.com>
2018-01-11 13:30:58 +0100

    Made some structural changes and added some JS

--- spel.js
+++ spel.js
@@ -1,15 +1,32 @@
-/*/
-
-document.getElementById("spelcanvas").addEventListener("mouseover", function () {
+/*Gingerbreadman on cursor when mouseover game canvas*/
+document.getElementById("spelCanvas").addEventListener("mouseover", function () {
 
 });
 
- function moveright() {
-    myGamePiece.speedX += 1;
+var modal = document.getElementById('simpleModal');
+var svgMap = document.getElementById('svgMap');
+var playAgainBtn = document.getElementById('playAgainBtn');
+var quitBtn = document.getElementById('quitBtn');
+
+
+/*Temporary button to open modal and opacity background*/
+svgMap.addEventListener('mouseover', openModal);
+
+/*Temporary function to close modal and opacity background*/
+playAgainBtn.addEventListener('click', closeModal);
+quitBtn.addEventListener('click', closeModal);
+
+
+/*Open modal function*/
+function openModal(){
+    modal.style.display = 'flex';
 }
 
-/*/
-
+/*Close modal function*/
+function closeModal() {
+    modal.style.display = 'none';
+    location.reload();
+}
 function play(){
     var audio = document.getElementById("audio");
     audio.play();
