42b521f986501f87b96e1ab2361c6ca63c0b3fcc
Simon Dellros <simondellros@Simons-MacBook-Air-2.local>
2018-01-19 08:35:20 +0100

    la till timer som score, onload på svgn

---
+++ score.js
@@ -0,0 +1,35 @@
+function timedText() {
+    setTimeout(myTimeout1, 100)
+    setTimeout(myTimeout2, 200)
+    setTimeout(myTimeout3, 300)
+    setTimeout(myTimeout4, 400)
+    setTimeout(myTimeout5, 500)
+    setTimeout(myTimeout6, 600)
+    setTimeout(myTimeout7, 700)
+    setTimeout(myTimeout8, 400)
+}
+
+function myTimeout1() {
+    document.getElementById("score").innerHTML = "1";
+}
+function myTimeout2() {
+    document.getElementById("score").innerHTML = "2";
+}
+function myTimeout3() {
+    document.getElementById("score").innerHTML = "3";
+}
+function myTimeout4() {
+    document.getElementById("score").innerHTML = "4";
+}
+function myTimeout5() {
+    document.getElementById("score").innerHTML = "5";
+}
+function myTimeout6() {
+    document.getElementById("score").innerHTML = "6";
+}
+function myTimeout7() {
+    document.getElementById("score").innerHTML = "7";
+}
+function myTimeout8() {
+    document.getElementById("score").innerHTML = "8";
+}
