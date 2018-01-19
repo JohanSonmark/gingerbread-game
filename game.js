d77df7a7a6df1da0e0627db37fd700d1a532c40b
Christoffer Nygren <christoffernygren90@hotmail.com>
2018-01-19 11:07:16 +0100

    Removed unecessary files and code, renamed certain files

--- game.js
+++ game.js
@@ -173,14 +173,6 @@ window.onload = function() {
 
 document.getElementById('countDown').addEventListener('click', startGame);
 
-/*
-function delay(ms) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(resolve, ms);
-    });
-}
-*/
-
 function startGame() {
     var count = 3;
     function counter() {
@@ -199,3 +191,40 @@ function startGame() {
     }
     counter();
 }
+
+/*
+function timedText() {
+    setTimeout(myTimeout1, 100)
+    setTimeout(myTimeout2, 200)
+    setTimeout(myTimeout3, 300)
+    setTimeout(myTimeout4, 400)
+    setTimeout(myTimeout5, 500)
+    setTimeout(myTimeout6, 600)
+    setTimeout(myTimeout7, 700)
+    setTimeout(myTimeout8, 400)
+}
+
+function myTimeout1() {
+    document.getElementById("score").innerHTML = "1";
+}
+function myTimeout2() {
+    document.getElementById("score").innerHTML = "2";
+}
+function myTimeout3() {
+    document.getElementById("score").innerHTML = "3";
+}
+function myTimeout4() {
+    document.getElementById("score").innerHTML = "4";
+}
+function myTimeout5() {
+    document.getElementById("score").innerHTML = "5";
+}
+function myTimeout6() {
+    document.getElementById("score").innerHTML = "6";
+}
+function myTimeout7() {
+    document.getElementById("score").innerHTML = "7";
+}
+function myTimeout8() {
+    document.getElementById("score").innerHTML = "8";
+}*/
